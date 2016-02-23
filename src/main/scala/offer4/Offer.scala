4d24d6973b009437426bb487fe035afdfaf27b00
Martin Gerlach <martin.gerlach@idealo.de>
2016-02-23 18:38:02 +0100

    start another attempt to generically get and modify object/map properties

--- src/main/scala/offer4/Offer.scala
+++ src/main/scala/offer4/Offer.scala
@@ -1,49 +1,59 @@
 package offer4
 
-import context.Context
+import context.{Context, Global}
 
 import scala.util.Try
 
+// TODO replace R, W by V and use Option[V] as return value for latest()
+
 trait Access[T, R, W] {
-  def get: R
 
-  def set(v: W): T
+  /**
+    * @return the most recent value passed to consume(W), if any.
+    */
+  def latest: R
 
-  def clear: T
-
-  def remove: T
+  /**
+    * Consumes a value by adding it to the underlying data structure of the current T
+    * @param v value
+    * @return new T containing v
+    */
+  def consume(v: W): T
 }
 
 case class MapAccess[K, V](m: Map[K, V], k: K) extends Access[Map[K, V], Option[V], V] {
-  def get = m.get(k)
 
-  def set(v: V) = m updated(k, v)
+  def latest = m.get(k)
 
-  def clear = Map()
-
-  def remove = m - k
+  def consume(v: V) = m updated(k, v)
 }
 
 case class ListAccess[V](l: List[V]) extends Access[List[V], Option[V], V] {
-  def get = Try(Option(l.last)).getOrElse(None)
 
-  def set(v: V) = l :+ v // append
+  def latest = Try(Option(l.last)).getOrElse(None) // return last, no/empty list yields in None
 
-  def clear = Nil
-
-  def remove = Try(l.init).getOrElse(Nil) // remove last, empty stays empty
+  def consume(v: V) = l :+ v // append
 }
 
-case class CompositeAccess[T, R1, W1, R2, W2](a1: Access[T, R1, W1], inner: Access[T, R1, W1] => Access[W1, R2, W2]) extends Access[T, R2, W2] {
-  lazy val a2: Access[W1, R2, W2] = inner(a1)
+// TODO if we are more relaxed about types, can the composites be condensed into some function using recursion?
 
-  def get = a2.get
+case class CompositeAccess2[T, R1, W1, R2, W2](a1: Access[T, R1, W1], next: Access[T, R1, W1] => Access[W1, R2, W2]) extends Access[T, R2, W2] {
+  lazy val a2: Access[W1, R2, W2] = next(a1)
 
-  def set(v: W2) = a1.set(a2.set(v))
+  def latest = a2.latest
 
-  def clear = a1.clear
+  def consume(v: W2) = a1.consume(a2.consume(v))
+}
 
-  def remove = a1.remove
+case class CompositeAccess3[T, R1, W1, R2, W2, R3, W3](a1: Access[T, R1, W1], next1: Access[T, R1, W1] => Access[W1, R2, W2], next2: Access[W1, R2, W2] => Access[W2, R3, W3]) extends Access[T, R3, W3] {
+
+  lazy val a2: Access[W1, R2, W2] = next1(a1)
+
+  lazy val a3: Access[W2, R3, W3] = next2(a2)
+
+  def latest = a3.latest
+
+  def consume(v: W3) = a1.consume(a2.consume(a3.consume(v)))
 }
 
 /**
@@ -55,32 +65,67 @@ case class Offer(m: Map[String, Option[Any]]) {
 
   lazy val sku = TopLevelAccess[String]("sku")
 
+  lazy val title = TopLevelAccess[Map[Context, String]]("title")
+
+  // TODO define the composites as types
+
   def title(c: Context): Access[Offer, Option[String], String] =
-    CompositeAccess[Offer, Option[Option[Map[Context, String]]], Map[Context, String], Option[String], String](
-      TopLevelAccess[Map[Context, String]]("title"),
-      tla => MapAccess[Context, String](tla.get.getOrElse(Option(Map[Context, String]())).get, c))
+    CompositeAccess2[Offer, Option[Map[Context, String]], Map[Context, String], Option[String], String](
+      title,
+      tla => MapAccess[Context, String](tla.latest.getOrElse(Map[Context, String]()), c))
 
-  lazy val categoryPaths: Access[Offer, Option[String], String] =
-    CompositeAccess[Offer, Option[Option[List[String]]], List[String], Option[String], String](
-      TopLevelAccess[List[String]]("categoryPaths"),
-      tla => ListAccess[String](tla.get.getOrElse(Option(Nil)).get))
+  lazy val categoryPaths = TopLevelAccess[List[String]]("categoryPaths")
 
-  def images(c: Context): Access[Offer, Option[String], String] =
-    CompositeAccess[Offer, Option[Option[Map[Context, List[String]]]], List[String], Option[String], String](
-      CompositeAccess[Offer, Option[Option[Map[Context, List[String]]]], Map[Context, List[String]], Option[List[String]], List[String]](
-        TopLevelAccess[Map[Context, List[String]]]("images"),
-        tla => MapAccess[Context, List[String]](tla.get.getOrElse(Option(Map[Context, List[String]]())).get, c)),
-      ca => ListAccess[String](ca.get.getOrElse(Option(Map[Context, List[String]]())).get.getOrElse(c, Nil))
-    )
+  lazy val categoryPath: Access[Offer, Option[String], String] =
+    CompositeAccess2[Offer, Option[List[String]], List[String], Option[String], String](
+      categoryPaths,
+      tla => ListAccess[String](tla.latest.getOrElse(Nil)))
 
-  case class TopLevelAccess[T](k: String) extends Access[Offer, Option[Option[T]], T] {
-    def get = m.get(k).asInstanceOf[Option[Option[T]]]
+  lazy val images = TopLevelAccess[Map[Context, List[String]]]("images")
 
-    def set(v: T) = Offer(m updated(k, Option(v)))
+  def images(c: Context): Access[Offer, Option[List[String]], List[String]] =
+    CompositeAccess2[Offer, Option[Map[Context, List[String]]], Map[Context, List[String]], Option[List[String]], List[String]](
+      images,
+      tla => MapAccess[Context, List[String]](tla.latest.getOrElse(Map[Context, List[String]]()), c))
+
+  def image(c: Context): Access[Offer, Option[String], String] =
+    CompositeAccess3[Offer, Option[Map[Context, List[String]]], Map[Context, List[String]], Option[List[String]], List[String], Option[String], String](
+      images,
+      tla => MapAccess[Context, List[String]](tla.latest.getOrElse(Map[Context, List[String]]()), c),
+      a2 => ListAccess[String](a2.latest.getOrElse(Nil)))
+
+  // GENERIC ACCESS (e.g., CSV mapping) for sequentially adding single values
+
+  // TODO add optional map key as second function param
+  // TODO this is for strings (CSV mapping only) -> how can we add conversion functions, e.g. for prices
+
+  lazy val all: Map[String, Option[Context] => Any] = Map(
+    sku.k -> (c => sku),
+    title.k -> (c => title(c.getOrElse(Global))),
+    categoryPaths.k -> (c => categoryPath),
+    images.k -> (c => image(c.getOrElse(Global))))
+
+  def genericAccessFor[T](k: String)(c: Option[Context]) = all(k)(c).asInstanceOf[Access[Offer, Option[String], String]]
+
+  case class TopLevelAccess[T](k: String) extends Access[Offer, Option[T], T] {
+
+    /**
+      * Returns an Option containing the most recent value passed to consume(T), if any.
+      * A return value of None (Option) can mean that nothing has been set OR clear() was called to clear the property in OS.
+      * Use isClear() to find out (more explicit and nicer with composite Access objects than working with Optional of Optional).
+      *
+      * @return latest value for our key (offer property), or None if the property is to be ignored or cleared (use isClear() to find out)
+      */
+    def latest = m.getOrElse(k, None).asInstanceOf[Option[T]]
+
+    def consume(v: T) = Offer(m updated(k, Option(v))) // outer Option indicates modification of property, inner indicates that a value is set
 
     def clear = Offer(m updated(k, None))
 
-    def remove = Offer(m - k)
+    // check for k -> None (indicated by Some(None) returned by m.get(k)) which indicates to clear the property in OS
+    def isClear = m.get(k).exists(o => o.isEmpty)
+
+    def remove = Offer(m - k) // if the complete binding is remove, the property is ignored in OS
   }
 
 }
--- src/main/scala/offer4/sandbox.sc
+++ src/main/scala/offer4/sandbox.sc
@@ -1,18 +1,44 @@
 import context._
-import offer4.{ListAccess, MapAccess, Offer}
+import offer4.Offer
+
+import scala.util.Try
 
 val o1 = Offer.create
 
-val o2 = o1.sku.set("SKU")
+val o2 = o1.sku.consume("SKU")
 
-val o3 = o2.categoryPaths.set("C1")
+o2.categoryPaths.latest
+o2.categoryPath.latest
+o2.categoryPaths.isClear
 
-val o4 = o3.categoryPaths.set("C2")
+val o3 = o2.categoryPath.consume("C1")
+o3.categoryPaths.latest
+o3.categoryPath.latest
+val o4 = o3.categoryPath.consume("C2")
+// lists -> only the last element is returned...
+o4.categoryPaths.latest
+o3.categoryPaths.latest
 
-o4.categoryPaths.get
+val o3c = o3.categoryPaths.clear
+o3c.categoryPaths.latest
+o3c.categoryPaths.isClear
+o3c.m.get("categoryPaths")
 
-o3.categoryPaths.get
 
-val o5 = o4.title(DE).set("Titel").title(Global).set("title")
+val o5 = o3.title(DE).consume("Titel").title(Global).consume("title")
+o5.title(DE).latest.getOrElse("Meeeeeep!")
+Try(o5.TopLevelAccess[Map[Context,String]]("title").latest.get.get(DE).get).getOrElse("Meeeeeep!")
+val o6 = o5.image(Global).consume("http://image1_0")
+  .image(Global).consume("http://image2_0")
+  .image(DE).consume("http://image1_DE")
+o6.images.clear
+o6.images.remove
+o6.images.isClear
 
-o5.TopLevelAccess[Map[Context,String]]("title").get.get.get.get(DE).get
+Try(o6.images.latest.get).getOrElse("Meeeeeep!")
+
+// generic
+
+val imagesAccess = o6.genericAccessFor[String]("images") _
+
+imagesAccess(Option(DE)).consume("yet another image")
